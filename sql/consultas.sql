15a78b9e77351161edbd8e7a11c6b7f16fbe81e8
Francisca Brinckfeldt <francisca.brinckfeldt@gmail.com>
2019-09-15 17:32:21 -0300

    Orden en ficheros

---
+++ sql/consultas.sql
@@ -0,0 +1,15 @@
+SELECT dog_name, dog_status FROM dogs WHERE dog_id = 4;
+
+SELECT * FROM customers_dogs;
+
+SELECT dog_name, dog_status FROM dogs WHERE dog_id = 4;
+SELECT * FROM customers_dogs;
+    
+SELECT * FROM breeds;
+
+SELECT dog_id, dog_name, dog_age, dog_half_breed, dog_breed, dog_photo 
+FROM dogs
+WHERE dog_status = dog_status;
+
+SELECT dog_id, dog_name, dog_age, dog_half_breed, dog_breed, dog_photo 
+FROM dogs;
