68abe83d2f2c4309b31bacb574820873adac91f6
Francisca Brinckfeldt <francisca.brinckfeldt@gmail.com>
2019-09-15 19:26:06 -0300

    Modificando las vistas

---
+++ admin/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Manada</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Manada</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Perros <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Clientes</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cerrar Sesión</a>
+                    </li>
+                </ul>
+            </div>
+        </div>    
+    </nav>
+
+    <section class="container">
+        <br><h1>Bienvenido</h1><br>
+
+        <div class="row">
+            <div class="col 4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Activos</h5>
+                        <p class="card-text">15</p>
+                        <a href="#" class="btn btn-primary">Ver más</a>
+                    </div>  
+                 </div>
+            </div>
+            <div class="col 4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Adoptados</h5>
+                        <p class="card-text">35</p>
+                        <a href="#" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col 4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Inactivos</h5>
+                        <p class="card-text">21</p>
+                        <a href="#" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,30 +1,38 @@
-<?php
-    //adopta.elpoderdelamanada.org
-    $array = [["id"=>1, "name"=>"Doggo"], ["id"=>2, "name"=>"Loco"], ["id"=>3, "name"=>"Huba"]];
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Manada Adopciones</title>
 </head>
 <body>
-    <table>
-        <thead>
-            <th>ID</th>
-            <th>Name</th>
-        </thead>
-        <tbody>
-            <?php foreach($array as $item): ?>
-            <tr>
-                <td><?php echo $item['id'] ?></td>
-                <td><?php echo $item['name'] ?></td>
-            </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+
+    <nav class="navbar navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand">Manada</a>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ir a nuestro sitio web</button>
+        </div>
+    </nav>
+
+    <div class="container">
+        <br><h1>Adopta un cachupino</h1><br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p><br>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Nombre</h5>
+                <p class="card-text">
+                    Sexo: <br>
+                    Edad: <br>
+                    Raza: <br>
+                </p>
+                <a href="#" class="btn btn-primary">Ver más información del cachupino</a>
+            </div>
+        </div>
+    </div>  
+    
+    
 </body>
 </html>
